a9c5f83ad81bfc850199aee6f34fd563ee71b9c9
pcRipper <maximuspro100@gmail.com>
2023-12-19 22:53:34 +0200

    mc

---
+++ bin/Headers/CornderDetectionAlgorithms.hpp
@@ -0,0 +1,6 @@
+#include <math.h>
+#include <iostream>
+
+void someFunc(){
+    std::cout << "Hello world\n";
+}
--- main.cpp
+++ main.cpp
@@ -2,22 +2,41 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include "bin/Headers/CornderDetectionAlgorithms.hpp"
+
 int main(int argc,char** argv){
 
-    cv::Mat image = cv::imread("C://Users//Maxim//Pictures//bubble_cat.jpg");
+    using namespace std;
 
-    // Check if the image was successfully loaded
-    if (image.empty()) {
-        std::cerr << "Error: Could not open or find the image.//n";
-        return -1;
+    try{
+
+        someFunc();
+
+        cout << "Starting a program!\n";
+        cout << "Loading an image...\n";
+
+        const cv::String IMG_PATH = "D://code//cpp//projects//cmake_corner_detection//images//bubble_cat.jpg";
+        cout << IMG_PATH << '\n';
+        cv::Mat image = cv::imread(IMG_PATH);
+
+        // Check if the image was successfully loaded
+        if (image.empty()) {
+            std::cerr << "Error: Could not open or find the image.\n";
+            return -1;
+        }
+
+        // Display the image in a window
+        cv::imshow("Image", image);
+
+        // Wait for a key press and then close the window
+        cv::waitKey(0);
+        cv::destroyAllWindows();
     }
+    catch(const exception& ex) {
 
-    // Display the image in a window
-    cv::imshow("Image", image);
+        cout << ex.what() << '\n';
 
-    // Wait for a key press and then close the window
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    }
 
     return 0;
 }
